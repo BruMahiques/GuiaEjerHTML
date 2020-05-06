222a0ab79e7769e8929fc77d1d21b41c1081836f
Bruno Mahiques <bruu_12496@hotmail.com>
2020-05-06 20:31:32 -0300

    Solucion nueva de ejercicios

---
+++ EjerciciosHTML/EjerciciosCSS/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjerciciosCSS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio1()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio2()
+        {
+            return View();
+        }
+
+    }
+}
